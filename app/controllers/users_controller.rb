9b093b4ec670871effeb7743818ff549fbfcfa89
Matthew Harrilal <matthewharrilal@gmail.com>
2018-01-25 11:46:00 -0800

    Finished the documentation for the user if they try to sign up with invalid credentials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,8 @@ end
 require 'rails_helper'
 
 RSpec.describe "UserControllers", type: :request do
+
+  # TEST FOR LOGGING A USER IN
   describe "GET /user_controllers" do
 
     # What this is essentially doing is that is saying before we log in the user that the state of the application should be unauthorized therefore the user should not be
@@ -103,22 +105,32 @@ RSpec.describe "UserControllers", type: :request do
 
   # Test signing up a user
   describe "POST /user_controllers" do
+
+    # So these are the tests for signing a user up
     context "valid params" do
       before {
+        # So before we sign  a user in this is the input that the user is going to be the credentials that are assigned below
         valid_params = {name: "Eliel", email: "eliel@test.com", password: "testpassword"}
         post "/users", params: valid_params
       }
 
+      # If the credentials are accepted then what we are essentially doing is that we are going to send back a succesfull response 
       it "creates and sends success of creating a user with valid params" do
         expect(response).to be_success
       end
     end
+
+    # Now that we have moved in from the creation of the succesful users we are switching contexts to the effect that if the user is passing in invalid parameters 
     context "invalid params" do
+
+      # This is before the user tries to sign in and if the user tries to sign in using the invalid parameters which in this example below what is happening is that the user
+      # is trying to pass in the parameters email and password which are invalid then we are going to send back a unsuccesful response  which is going to say invalid parameters
       before {
         invalid_params = {email: "eliel@test.com", password: "testpassword"}
         post "/users", params: invalid_params
       }
 
+      # And the response would be unsucessful and send back a 400
       it "should fail and send 400" do
         expect(response).to_not be_success
       end
